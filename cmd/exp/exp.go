796e73e868cc4e3fe8b42ba7f24ca53b8e76f706
Jason Diwa <d.jason.mail.01@gmail.com>
2023-08-25 09:50:39 -0700

    More postgres, querying orders

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -15,6 +15,13 @@ type PostgresConfig struct {
 	SSLMode  string
 }
 
+type Order struct {
+	ID          int
+	UserID      int
+	Amount      int
+	Description string
+}
+
 func (cfg PostgresConfig) String() string {
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
 }
@@ -64,19 +71,54 @@ func main() {
 	//	panic(err)
 	//}
 	//fmt.Println("User created, id =", id)
-	id := 10
-	row := db.QueryRow(`
-		SELECT name, email
-		FROM users
-		WHERE id=$1;`, id)
-	var name, email string
-	err = row.Scan(&name, &email)
-	if err == sql.ErrNoRows {
-		fmt.Println("Error, no rows!")
-	}
+	//id := 10
+	//row := db.QueryRow(`
+	//	SELECT name, email
+	//	FROM users
+	//	WHERE id=$1;`, id)
+	//var name, email string
+	//err = row.Scan(&name, &email)
+	//if err == sql.ErrNoRows {
+	//	fmt.Println("Error, no rows!")
+	//}
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Printf("User info: name=%s, email = %s\n", name, email)
+	//userID := 2
+	//for i := 1; i <= 5; i++ {
+	//	amount := i * 100
+	//	desc := fmt.Sprintf("Fake order #%d", i)
+	//	_, err := db.Exec(`
+	//INSERT INTO orders(user_id, amount, description)
+	//VALUES($1, $2, $3)`, userID, amount, desc)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//fmt.Println("Created fake orders.")
+	var orders []Order
+	userID := 2
+	rows, err := db.Query(`
+		SELECT id, amount, description
+		FROM orders
+		WHERE user_id=$1`, userID)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("User info: name=%s, email = %s\n", name, email)
-	defer db.Close()
+	for rows.Next() {
+		var order Order
+		order.UserID = userID
+		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Orders:", orders)
+	defer rows.Close()
 }
